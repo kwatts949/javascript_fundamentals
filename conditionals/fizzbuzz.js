d6aa1b3b03306fa31f1dc71d61f55b8cb07943d9
Kayleigh Watts <kwatts949@gmail.com>
2022-11-14 13:41:37 +0000

    Add conditionals exercises

---
+++ conditionals/fizzbuzz.js
@@ -0,0 +1,15 @@
+const fizzBuzz = (number) => {
+  if (number % 15 === 0) {
+      return 'FizzBuzz';
+  } else if (number % 5 === 0) {
+      return 'Buzz';
+  } else if (number % 3 == 0) {
+      return 'Fizz';
+  } else {
+      return number;
+  }
+}
+
+module.exports = fizzBuzz;
+
+// const fizzBuzz = require('./fizzBuzz')
