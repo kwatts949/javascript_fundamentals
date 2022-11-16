1291175981c8088b2747f6b6f0141587299ce1e4
Kayleigh Watts <kwatts949@gmail.com>
2022-11-16 09:28:47 +0000

    Rearrange directories

--- phase_2/search_function/searchCandies.js
+++ phase_2/search_function/searchCandies.js
@@ -24,25 +24,10 @@ const searchCandies = (string, maxPrice) => {
 }
 
 module.exports = searchCandies;
+
 /*
 You'll test-drive a function searchCandies that takes the following two arguments: a search string and a maximum price — and returns only the names of candies matching these criteria:
 
 the candy name starts by the search string
 the candy price is less than the maximum price
-
-const candies = [
-  { name: 'Aero', price: 1.99 },
-  { name: 'Skitties', price: 2.99 },
-  { name: 'Mars', price: 1.49 },
-  { name: 'Maltesers', price: 3.49 },
-  { name: 'Skittles', price: 1.49 },
-  { name: 'Starburst', price: 5.99 },
-  { name: 'Ricola', price: 1.99 },
-  { name: 'Polkagris', price: 5.99 },
-  { name: 'Pastila', price: 4.99 },
-  { name: 'Mentos', price: 8.99 },
-  { name: 'Raffaello', price: 7.99 },
-  { name: 'Gummi bears', price: 10.99 },
-  { name: 'Fraise Tagada', price: 5.99 }
-];
 */
