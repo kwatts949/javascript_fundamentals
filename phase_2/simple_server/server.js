69aab1ece19bf83b2368762fc55d74b7eab0652a
Kayleigh Watts <kwatts949@gmail.com>
2022-11-17 16:24:23 +0000

    simple server setup

---
+++ phase_2/simple_server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+let counter = 0;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Kay!");
+});
+
+app.get("/counter", (req, res) => {
+  res.send(`${counter}`);
+});
+
+app.post("/counter", (req, res) => {
+  counter += 1;
+  res.send();
+});
+
+app.post("/delete", (req, res) => {
+  counter = 0;
+  res.send();
+});
+
+console.log(`Server listening on localhost:${port}`);
+app.listen(port);
